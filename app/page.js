4ce7418efb7174a63fa48aa411bba0da0740677e
raymo7 <112167108+raymo7@users.noreply.github.com>
2026-08-09 15:07:01 +0530

    Redesigned

--- app/page.js
+++ app/page.js
@@ -1,31 +1,3 @@
-'use client';
-
-import SiteHeader from '../components/SiteHeader';
-import Hero from '../components/Hero';
-import PricingSection from '../components/PricingSection';
-import Gallery from '../components/Gallery';
-import BookingForm from '../components/BookingForm';
-import StandalonePayment from '../components/StandalonePayment';
-import EnquiryForm from '../components/EnquiryForm';
-import SiteFooter from '../components/SiteFooter';
-import MobileBottomNav from '../components/MobileBottomNav';
-import BackToTop from '../components/BackToTop';
-
-export default function Home() {
-  return (
-    <>
-      <SiteHeader />
-      <main>
-        <Hero />
-        <PricingSection />
-        <Gallery />
-        <BookingForm />
-        <StandalonePayment />
-        <EnquiryForm />
-      </main>
-      <SiteFooter />
-      <MobileBottomNav />
-      <BackToTop />
-    </>
-  );
-}
+import PageShell from '../components/PageShell';
+import HomeSections from '../components/HomeSections';
+export default function Home(){return <PageShell><HomeSections/></PageShell>}
