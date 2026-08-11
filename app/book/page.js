c99a27ad7edfc512e2aa0b10993e4286caf72d7a
raymo7 <112167108+raymo7@users.noreply.github.com>
2026-08-12 01:19:49 +0530

    V3

--- app/book/page.js
+++ app/book/page.js
@@ -1,4 +1,12 @@
 import PageShell from '../../components/PageShell';
 import BookingForm from '../../components/BookingForm';
-export const metadata={title:'Book Vehicle Care | Aqua Haul'};
-export default function BookPage(){return <PageShell><BookingForm/></PageShell>}
+
+export const metadata = { title: 'Book Vehicle Care | Aqua Haul' };
+
+export default function BookPage() {
+  return (
+    <PageShell hideFooter>
+      <BookingForm />
+    </PageShell>
+  );
+}
